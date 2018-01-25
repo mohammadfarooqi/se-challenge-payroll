0d61a79df9b1062e20795ae40f911e180419a0a3
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-01-25 13:01:25 -0500

    added enabled cors, multer, and /upload post

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const multer = require('multer');
+
+// directory for the uploads
+const UPLOAD_DIR = './uploads/';
+
+//define the type of upload multer would be doing and pass in its destination, single file with the name file
+const upload = multer({ dest: UPLOAD_DIR }).single('file');
+
+//allow cross origin requests
+app.use(function(req, res, next) { 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // setup logger
 app.use(morgan('dev'));
@@ -17,7 +32,22 @@ var router = express.Router();
 
 // test route to make sure everything is working http://localhost:8080/api
 router.get('/', function(req, res) {
-  res.json({ message: 'Hello World!' });   
+  res.json({ message: 'Test Hello World Route!' });   
+});
+
+// file upload function route
+router.post('/upload', function (req, res, next) {
+  let path = '';
+  upload(req, res, function (err) {
+     if (err) {
+       // An error occurred when uploading
+       console.log(err);
+       return res.status(422).send('an Error occured');
+     }
+
+     path = req.file.path;
+     return res.send('Upload Completed for ' + path); 
+  });     
 });
 
 // register routes... routes prefixed /api

fd2a3acaed932bc9660cfa05a594b5a8a1c7ac65
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-01-26 11:52:18 -0500

    added comments and completed upload route

--- server.js
+++ server.js
@@ -48,22 +48,32 @@ router.post('/upload', (req, res, next) => {
       return res.status(422).json({ message: 'Error: Unable to upload file.' });
     }
 
+    // path of uploaded file, ie. uploads\ecad757bcfaec530978c32a81aa393ed
     path = req.file.path;
 
     let csvJSON;
 
     try {
+      // read csv file and conver to JSON obj 
       csvJSON = await helper.csvToJSON(path);
     } catch (err) {
       console.log('Error in CSVJSON func', JSON.stringify(err));
     }
 
+    // check if last obj in array contains report id and report number
+    // note that report id comes under date column in csv and report id (the number)
+    // comes under hours worked column in csv
     if (csvJSON.length > 0 && csvJSON[csvJSON.length - 1] && csvJSON[csvJSON.length - 1].date === 'report id' && csvJSON[csvJSON.length - 1]['hours worked']) {
       try {
+        // check and see if report id we have in uploaded file exists in db (returns true/false)
         const result = await helper.checkReportIdExists(csvJSON[csvJSON.length - 1]['hours worked']);
         // console.log(result);
 
+        // if report id does exist in db then send a 409
         if (result) {
+          // delete file 
+          helper.deleteFile(path);
+        
           return res.status(409).json({ message: 'Error: report id in uploaded CSV already exists.' });
         }
       } catch (err) {
@@ -71,9 +81,16 @@ router.post('/upload', (req, res, next) => {
       }
     }
 
+    try {
+      // at this point, its a new report, insert rows to database
+      const insert_result = await helper.insertCSVToDB(csvJSON);
+      console.log('insert result = ', insert_result);
 
-    // delete file 
-    helper.deleteFile(path);
+      // delete file 
+      helper.deleteFile(path);
+    } catch (err) {
+      console.log('Error in insertCSVToDB func', JSON.stringify(err));
+    }
 
     // return res.send('Upload Completed for ' + path); 
     return res.json({ message: 'Upload Completed for ' + path, csvJSON });

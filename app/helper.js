23271e362d05fae7582088a7a3adb94a6896a77e
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-01-26 11:52:57 -0500

    enabled longstacktraces on promises and completed insertcsvtodb func

--- app/helper.js
+++ app/helper.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const csvjson = require('csvjson');
 const path = require('path');
 const promise = require('bluebird');
+promise.config({longStackTraces: true});
 
 const options = {
   // Initialization Options
@@ -37,14 +38,7 @@ function csvToJSON(filepath) {
       delimiter : ',', // optional
       quote     : '"' // optional
     };
-   
-    /* 
-      for importing headers from different source you can use headers property in options 
-      var options = {
-        headers : "sr,name,age,gender"
-      };
-    */
-   
+    
     res(csvjson.toObject(data, options));
   });
 }
@@ -67,8 +61,49 @@ function checkReportIdExists(report_id) {
   });
 }
 
+function insertCSVToDB(csvJSON) {
+  return new Promise(async (res, rej) => {
+    const report_id = parseInt(csvJSON.pop()['hours worked']);
+    console.log('insertCSVToDB - adding values to table for report_id = ', report_id);
+    
+    let queries = 'insert into "employee_timecard" ("date_worked","hours_worked","employee_id","job_group","report_id") values ';
+    
+    csvJSON.map(item => {
+      queries += "(to_date('" + item['date'] + "', 'DD-MM-YYYY'), " + parseFloat(item['hours worked']) + ", " + 
+      parseInt(item['employee id']) + ", '" + item['job group'] + "', " + report_id + "),";
+      
+      // return {
+        //   date_worked: to_date(item['date'], 'DD-MM-YYYY'),
+        //   hours_worked: parseFloat(item['hours worked']),
+        //   employee_id: parseInt(item['employee id']),
+        //   job_group: item['job group'],
+        //   report_id
+        // };
+      });
+      
+      // remove last comma from queries
+      if (queries[queries.length - 1] === ',') {
+        queries = queries.slice(0, -1);
+      }
+
+      queries += ';';
+      
+      //const queries = pgp.helpers.insert(csvJSON, ['date_worked', 'hours_worked', 'employee_id', 'job_group', 'report_id'], 'employee_timecard');
+      console.log(queries);
+
+    try {  
+      const result = await db.any(queries);
+      res(result);
+    } catch (err) {
+      console.log('Error occured in checkReportIdExists: ', JSON.stringify(err));
+      rej('Error occured in checkReportIdExists' + JSON.stringify(err));
+    }
+  });
+}
+
 module.exports = {
   deleteFile,
   csvToJSON,
-  checkReportIdExists
+  checkReportIdExists,
+  insertCSVToDB
 }
